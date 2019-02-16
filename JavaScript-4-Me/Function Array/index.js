1e48e80df35f797e83ebb9060f4f58b1e7d4bf7c
NasipCOM <salmahunov@mail.ru>
2019-02-16 19:43:52 +0600

    Function array

---
+++ JavaScript-4-Me/Function Array/index.js
@@ -0,0 +1,19 @@
+var array = [1999, 1998, 2000, 2001];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+
+    }
+    return arrRes;
+}
+
+function calcAge(el) {
+    return 2020 - el;
+}
+
+
+var ages = arrayCalc(array, calcAge);
+
+console.log(ages);
