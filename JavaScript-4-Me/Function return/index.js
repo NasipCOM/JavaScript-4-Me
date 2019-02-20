1f5644730d6a92f6103eb0eda5a6926e5e43492e
NasipCOM <salmahunov@mail.ru>
2019-02-20 20:46:40 +0600

    Closers

--- JavaScript-4-Me/Function return/index.js
+++ JavaScript-4-Me/Function return/index.js
@@ -23,6 +23,33 @@ interview('Teacher')('Mark'); */
 // invoke functions
 
 
-(function(name){
+/* (function(name){
     console.log(name +  ' hello world');
-}('Mark'));
+}('Mark')); */
+
+
+//closures
+
+
+ function interview(job) {
+    var designer = 'can you tell us more about it';
+    if (job === 'designer') {
+        return function (name) {
+            console.log(`${name} ${designer}`)
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log(`${name} can you tell us more about Teachers`)
+        }
+    }
+    else return function (name) {
+        console.log(`${name} what do you do`)
+    }
+}
+
+const designerInter = interview('designer');
+designerInter('Yrys');
+
+const teacherInter = interview('teacher');
+teacherInter('Yrys');
+
